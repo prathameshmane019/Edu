661aaac74a21f5b50dd993d2a67597b20f461d3b
prathameshmane019 <maneprathamesh019@gmail.com>
2023-10-06 19:24:47 +0530

    api added for minnistry

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27c4ac7fc280fba1"; }
+/******/ 		__webpack_require__.h = function() { return "05835cd88db871d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
